b7f9068d5aa67b2a5f74b6ba4d527df8973473d0
Leo Gallucci <elgalu3@gmail.com>
2013-11-09 11:43:27 -0300

    WIP mysql

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,8 +5,10 @@ log_level                :info
 log_location             STDOUT
 node_name                "elgalu"
 client_key               "#{current_dir}/elgalu.pem"
+# client_key               "#{ENV['HOME']}/.ssh/id_rsa"
 validation_client_name   "rubywebs-validator"
 validation_key           "#{current_dir}/rubywebs-validator.pem"
+# validation_key           "#{ENV['HOME']}/.ssh/id_rsa.pub"
 chef_server_url          "https://api.opscode.com/organizations/rubywebs"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
